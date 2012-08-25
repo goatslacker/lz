3d64bfd84586f7b0c4f7e28f7c80f25e0d48d645
Josh Perez <josh@goatslacker.com>
2012-08-24 22:52:26 -0700

    Adds a step test to ranges

--- test/ranges-test.js
+++ test/ranges-test.js
@@ -14,3 +14,12 @@ exports.ranges = function (lz, assert) {
     [1, 4, 9, 16]
   )
 }
+
+exports.step = function (lz, assert) {
+  assert.deepEqual(
+    lz.range(1, 10)
+    .filter(function (n) { return n % 2 !== 0 })
+    .$(),
+    [1, 3, 5, 7, 9]
+  )
+}
