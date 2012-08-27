f958fe0cfe7a61f4e1bd20fcafa034cfc7a5e231
Josh Perez <josh@goatslacker.com>
2012-08-26 19:32:50 -0700

    Adds repeat tests

---
+++ test/repeat-test.js
@@ -0,0 +1,27 @@
+exports.repeat = function (lz, assert) {
+  assert.deepEqual(
+    lz
+    .repeat(1)
+    .take(3)
+    .$(),
+    [1, 1, 1]
+  )
+
+  assert.equal(
+    lz
+    .repeat(2)
+    .head(),
+    2
+  )
+}
+
+exports.chained = function (lz, assert) {
+  assert.equal(
+    lz
+    .repeat(3)
+    .map(function (n) { return n * 3 })
+    .take(2)
+    .last(),
+    9
+  )
+}
