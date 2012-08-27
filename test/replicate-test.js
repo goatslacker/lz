b6884eccc39dd518197cf28017789ad3b1ade3ef
Josh Perez <josh@goatslacker.com>
2012-08-26 19:43:13 -0700

    Adds replicate tests

---
+++ test/replicate-test.js
@@ -0,0 +1,18 @@
+exports.replicate = function (lz, assert) {
+  assert.deepEqual(
+    lz
+    .replicate(3, 1)
+    .$(),
+    [1, 1, 1]
+  )
+}
+
+exports.chained = function (lz, assert) {
+  assert.equal(
+    lz
+    .replicate(2, 3)
+    .map(function (n) { return n * 3 })
+    .head(),
+    9
+  )
+}
